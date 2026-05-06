7cb0ceae22cf7ad5cd11410f350a7265247afcc8
Tynahan <134694901+ThePott@users.noreply.github.com>
2026-05-06 16:02:35 +0900

    feat: hide overflow in input (#0)

--- src/shared/components/inputs/Input/Input.tsx
+++ src/shared/components/inputs/Input/Input.tsx
@@ -6,7 +6,7 @@ import type { ReactNode } from "react"
 import { Hstack } from "../../layouts"
 
 const inputVariants = cva(
-  "grow rounded-md transition items-center outline -outline-offset-1 bg-card text-text-primary disabled:text-text-disabled focus-within:shadow-box pr-lg",
+  "grow overflow-hidden rounded-md transition items-center outline -outline-offset-1 bg-card text-text-primary disabled:text-text-disabled focus-within:shadow-box pr-lg",
   {
     variants: {
       status: {
